02e105f1d274f2dcd878380714158c01a7da46c4
kn <kunyoz@hotmail.com>
2020-09-05 22:57:47 +0100

    Added local user id parameter

--- gpgdecrypt.sh
+++ gpgdecrypt.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
+set -e
+KEY_FINGERPRINT=$1
 1>&2 echo "Pipe in message, or paste and it ctrl-d when done"
 MESSAGE=$(cat)
 1>&2 echo -e "$MESSAGE"
-echo -e "$MESSAGE" | gpg --decrypt --armor
+echo -e "$MESSAGE" | gpg --decrypt -u "$KEY_FINGERPRINT"
 if [ $? -ne 0 ]; then
     1>&2 echo "Decryption failed"
     exit 1
